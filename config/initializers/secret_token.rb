86854f3144b3e424e87814c07facd018a4daf164
MrRoundTree <mrroundtree@mrroundtree-HP-Mini-210-1000.(none)>
2014-02-20 19:07:03 +0530

    Primo Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Relive::Application.config.secret_key_base = 'e1bff2137e449820847d108f0352d5400a916f71ddc965ffedb2b00d57868204900bee7a53df45bf1e388fb9ad128c46729c5ae88f7a09692fa829e5491cbdf6'
